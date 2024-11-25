1b2c24113d5df1bb87c2d514d2afea61f85adf8f
Harutaka TAKEUCHI <nametaketakewo@icloud.com>
2024-11-26 00:11:35 +0900

    docker_hmr

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: true,
+        strictPort: true,
+        hmr: {
+            clientPort: parseInt((process.env.PORT_PREFIX ?? '0') + '5173'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
