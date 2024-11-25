6ccd2224d3d3d389dac8e7fe51d87e59b64a6edf
Harutaka TAKEUCHI <nametaketakewo@icloud.com>
2024-11-26 00:11:35 +0900

    docker_hmr

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ import {
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        host: true,
+        strictPort: true,
+        hmr: {
+            clientPort: parseInt((process.env.PORT_PREFIX ?? '0') + '5173'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
